077894b2e754be4d1604c01e7e9f2da247b7f9d2
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2023-06-09 19:31:16 -0500

    agregamos el footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,58 @@
+'use client'
+
 import { FC } from 'react'
+import Link from 'next/link'
 
-interface Props {
-  title: string
-  description: string
-}
-
-const Footer: FC<Props> = () => {
+const Footer: FC = () => {
   return (
-    <footer>
-      <p></p>
+    <footer className='flex bg-black h-1/4 p-6 bottom-0  justify-between text-white'>
+      <section className="flex-1 px-2">
+        <h1 className='font-bold text-lg md:text-2xl'>Dev Mario</h1>
+        <p className='text-sm overflow-hidden'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </section>
+      <section className="flex-1 px-2">
+        <h1 className='font-bold text-lg md:text-2xl'>Links</h1>
+        <ul className='flex flex-col'>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline  text-sm md:text-xl' href="/">Home</Link>
+          </li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/about">About</Link>
+          </li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/contact">Contact</Link></li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/projects">Projects</Link>
+          </li>
+        </ul>
+      </section>
+      <section className="flex-1 px-2">
+        <h1 className='font-bold text-2xl'>Social</h1>
+        <ul className='flex flex-col'>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline  text-sm md:text-xl' href="/">Home</Link></li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/about">About</Link>
+          </li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/contact">Contact</Link></li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/projects">Projects</Link></li>
+        </ul>
+      </section>
+      <section className="flex-1 pl-2">
+        <h1 className='font-bold text-2xl'>Contact</h1>
+        <ul className='flex flex-col'>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline  text-sm md:text-xl' href="/">Home</Link></li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/about">About</Link></li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/contact">Contact</Link></li>
+          <li>
+            <Link className='px-2 cursor-pointer  no-underline text-sm md:text-xl' href="/projects">Projects</Link></li>
+        </ul>
+      </section>
     </footer>
   )
 }
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -24,13 +24,32 @@ const Testimonials = () => {
       url: '/testimonial.svg',
       name: 'John Doe'
     },
+    {
+      url: '/testimonial.svg',
+      name: 'John Doe'
+    },
+    {
+      url: '/testimonial.svg',
+      name: 'John Doe'
+    },
+    {
+      url: '/testimonial.svg',
+      name: 'John Doe'
+    },
+    {
+      url: '/testimonial.svg',
+      name: 'John Doe'
+    },
+    {
+      url: '/testimonial.svg',
+      name: 'John Doe'
+    },
   ]
 
   return (
     <>
       <h1 className='text-3xl md:text-5xl font-bold text-green-600 text-center my-10'>Testimonials</h1>
-
-      <div className='flex flex-col md:flex-row justify-around flex-wrap mx-auto my-6 mt-20'>
+      <div className='grid grid-cols-2 justify-center md:flex md:flex-row md:justify-around md:flex-wrap ,md:mx-auto md:my-6 md:mt-20'>
         {
           testimonialArray.map((testimonial, index) => (
             <TestimonialCard key={index} url={testimonial.url} name={testimonial.name} />
