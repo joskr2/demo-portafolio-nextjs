8dc153476db6723d62c0c202500a8bace12890a5
Josue Patricio Retamozo Vargas <josretamozovar@gmail.com>
2023-06-09 21:53:14 -0500

    cambios menores

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({
         <>
           {children}
         </>
-        <Footer title='Footer' description='demo footer' />
+        <Footer  />
       </body>
     </html>
   )
